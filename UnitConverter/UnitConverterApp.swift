03a90ad2aade6956b24f8e130ab6a0ae41f96b7a
jerdrb <jerdrb@gmail.com>
2022-06-12 13:38:14 -0500

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Jared Bell on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
